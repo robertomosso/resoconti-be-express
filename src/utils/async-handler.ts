e4d07529aeacf7ca7d8404c4e3a668573294781f
robmosso <roberto.mosso@eng.it>
2025-08-29 12:10:36 +0200

    aggiunta generics async handler, modifica script build per aggiornare client prisma in produzione

--- src/utils/async-handler.ts
+++ src/utils/async-handler.ts
@@ -1,13 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
-import { CustomRequest } from '../interfaces/custom-request.interface';
 
 /**
  * 
- * @param fn helper function per gestire i middleware async, anche per la gestione degli errori
+ * @param fn helper function per gestire i middleware async, anche per la gestione degli errori. 
+ * Con tipizzazione esplicita tramite generics può accettare tutti i tipi che estendono Request (come CustomRequest)
  * @returns
  */
-export function asyncHandler(fn: (req: Request | CustomRequest, res: Response, next: NextFunction) => Promise<any>) {
-    return (req: Request | CustomRequest, res: Response, next: NextFunction) => {
+export function asyncHandler<
+  Req extends Request = Request,
+  Res extends Response = Response
+>(
+    fn: (req: Req, res: Res, next: NextFunction) => Promise<any>
+) {
+    return (req: Req, res: Res, next: NextFunction) => {
         fn(req, res, next).catch(next);
     };
 }
