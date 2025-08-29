ae3df593bc5c6bee5f04eda5ec6e79cc08bc1da8
robmosso <roberto.mosso@eng.it>
2025-08-29 11:40:00 +0200

    modifica custom request interface

--- src/interfaces/custom-request.interface.ts
+++ src/interfaces/custom-request.interface.ts
@@ -1,6 +1,11 @@
-import { Request as ExpressRequest } from 'express';
+import { Request } from 'express';
 
-export interface CustomRequest extends ExpressRequest {
-    userId?: string;
-	fileId?: string;
+export interface CustomRequest<
+  P = any,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
+  userId?: string;
+  fileId?: string;
 }
